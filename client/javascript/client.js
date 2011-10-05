0fb2741eef1b248a895573be06d404c994b94759
Sean Lee <selee@ign.com>
2011-10-05 15:21:19 -0700

    Reorganizing

---
+++ client/javascript/client.js
@@ -0,0 +1,36 @@
+// Register the variable that will hold the Twilio Connection
+var connection ;
+	
+
+  $(document).ready(function(){
+		//Immediately set the status
+		$('#status').text('Setting up');
+
+		// TODO the token is set by the template
+		console.log('Setting up Twilio with token ' + token);
+    Twilio.Device.setup(token);
+
+    Twilio.Device.ready(function (device) { 
+      $('#status').text('Ready to join conference');
+      Twilio.Device.connect();
+    });
+                        
+    Twilio.Device.offline(function (device) { 
+      $('#status').text('Connection offline');
+    });
+                        
+    Twilio.Device.error(function (error) { 
+      $('#status').text(error);
+    });
+                        
+    Twilio.Device.connect(function (conn) { 
+			connection = conn;
+      $('#status').text("Successfully joined conference");
+    });
+
+    Twilio.Device.disconnect(function (conn) { 
+      $('#status').text("Disconnected");
+    });
+                                
+  });
+
